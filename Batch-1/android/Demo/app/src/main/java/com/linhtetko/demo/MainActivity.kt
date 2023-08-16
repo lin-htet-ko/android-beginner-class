36a2d152d7e511f79d740a051a6f20f0d5087731
linhtetko101 <linhtetko@myansis.com>
2023-08-16 20:31:04 +0630

    Simple Wallpaper Design

--- Batch-1/android/Demo/app/src/main/java/com/linhtetko/demo/MainActivity.kt
+++ Batch-1/android/Demo/app/src/main/java/com/linhtetko/demo/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.linhtetko.demo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+/*
+    1 - Activity - UserInterface
+    2 - BroadcastReceiver
+    3 - Services
+    4 - Content Provider
+ */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // View -> ViewGroup -> View, View, View
+    }
+
+    fun clickMeClick(view: View) {
+        Toast.makeText(this, "User click button!", Toast.LENGTH_LONG).show()
     }
 }
